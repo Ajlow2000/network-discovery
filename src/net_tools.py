598f9eab348868e549630f5fe406353c2e59717a
Ajlow2000 <ajlow2000@gmail.com>
2020-11-25 12:31:06 -0600

    DRY

--- src/net_tools.py
+++ src/net_tools.py
@@ -1,19 +1,21 @@
 import platform
 import socket
 import subprocess
+from scapy.all import Ether, ARP, srp
 from datetime import datetime, time
 
 from networkADT import Network
 
-def pingsweep(network: Network, STDOUT=True):
-    def detect_ping_cmd():
-        cmd = {}
-        cmd["linux"] = "ping -c 1 "
-        cmd["windows"] = "ping -n 1 -w 500 "
-        os = platform.system().lower()
-        return cmd[os]
+def scan(network: Network, protocol="icmp", ports=None, STDOUT=True):
 
     def ping_ip(ip):
+        def detect_ping_cmd():
+            cmd = {}
+            cmd["linux"] = "ping -c 1 "
+            cmd["windows"] = "ping -n 1 -w 500 "
+            os = platform.system().lower()
+            return cmd[os]
+
         try:
             result = subprocess.check_output(detect_ping_cmd() + str(ip), shell=True).decode()
             if "unreachable" or "Sent = 1, Received = 0" not in result:
@@ -26,15 +28,20 @@ def pingsweep(network: Network, STDOUT=True):
             pass
 
     try:
-        ping_start = datetime.now()
-        if STDOUT: print("Pingsweep Initialized")
+        start = datetime.now()
+        if STDOUT: print("Scan Initialized with " + protocol.upper())
         for ip in network.address_range:
-            ping_ip(ip)
+            if protocol == "icmp":
+                ping_ip(ip)
+            elif protocol == "arp":
+                arp_ip(ip)
+            else:
+                raise ValueError("Unrecognized Protocol: " + protocol)
     except KeyboardInterrupt:
         if STDOUT: print("Cancelling...\n")
     finally:
-        ping_end = datetime.now()
-        ping_time = ping_end - ping_start
-        network.log_scan("pingsweep", time_elapsed=str(ping_time), time_completed=str(ping_end))
-        if STDOUT: print("Completed in " + str(ping_time))
+        end = datetime.now()
+        elapsed = end - start 
+        network.log_scan(protocol, time_elapsed=str(elapsed), time_completed=str(end))
+        if STDOUT: print("Completed in " + str(elapsed))
         return network
--- src/network_discovery.py
+++ src/network_discovery.py
@@ -1,3 +1,7 @@
 import json
 from networkADT import Network
-from net_tools import pingsweep, arpscan, portscan
+from net_tools import scan
+
+n = Network("192.168.50.0/24")
+n = scan(n)
+print(n.json())
